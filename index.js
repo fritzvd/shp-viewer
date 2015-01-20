0ec2e51152edfd0265fc2d3fcf59298d0ee11662
fritzvd <f.vandeventer@gmail.com>
2015-01-20 10:58:23 +0100

    all da tings init

---
+++ index.js
@@ -0,0 +1,30 @@
+var shp = require('shpjs'),
+    L = require('leaflet');
+    shpEl = document.getElementById("shapefile");
+
+
+
+var addToMap = function (gj) {
+  L.geoJson(gj.features).addTo(map);
+}
+
+shpEl.addEventListener("change", handleFiles, false);
+function handleFiles() {
+  var fileList = this.files; /* now you can work with the file list */
+  var reader = new FileReader();
+  reader.onloadend = function (progress, buff) {
+    var zip = shp.parseZip(this.result);
+    addToMap(zip);
+
+  };
+  reader.readAsArrayBuffer(this.files[0]);
+};
+
+var map = L.map('map', {
+  center: [51.0, 4.1],
+  zoom: 10
+});
+
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
