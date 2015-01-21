edfff32c736304581c925bc7fde10ad8ec6fcefa
fritzvd <f.vandeventer@gmail.com>
2015-01-21 20:21:01 +0100

    viewer with tables

--- index.js
+++ index.js
@@ -4,10 +4,52 @@ var shp = require('shpjs'),
 
 
 
+L.Icon.Default.imagePath = 'http://cdn.leafletjs.com/leaflet-0.7.3/images';
 var addToMap = function (gj) {
-  var geojsonLayer = L.geoJson(gj.features).addTo(map);
+  var geojsonMarkerOptions = {
+    radius: 8,
+    fillColor: "#ff7800",
+    color: "#000",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.8
+  };
+
+  var geojsonLayer = L.geoJson(gj.features, {
+    pointToLayer: function (feature, latlng) {
+      return L.circleMarker(latlng, geojsonMarkerOptions);
+    }
+  }).addTo(map);
   map.fitBounds(geojsonLayer.getBounds());
 }
+
+var createTd = function (prop, tr) {
+  var td = document.createElement("td");
+  td.innerHTML = prop;
+  tr.appendChild(td);
+  return tr;
+};
+
+var addTable = function (gj) {
+  var table = document.getElementById('table');
+  var thead = document.createElement('thead');
+  Object.keys(gj.features[0].properties).forEach(function (prop) {
+    var th = document.createElement('th');
+    th.innerHTML = prop;
+    thead.appendChild(th);
+  });
+  var tbody = document.createElement('tbody');
+  gj.features.forEach(function (feature) {
+    var tr = document.createElement("tr");
+    for (i in feature.properties) {
+      var prop = feature.properties[i];
+      var el = createTd(prop, tr);
+    }
+    tbody.appendChild(tr);
+  });
+  table.appendChild(thead);
+  table.appendChild(tbody);
+};
 
 shpEl.addEventListener("change", handleFiles, false);
 function handleFiles() {
@@ -16,6 +58,7 @@ function handleFiles() {
   reader.onloadend = function (progress, buff) {
     var zip = shp.parseZip(this.result);
     addToMap(zip);
+    addTable(zip);
 
   };
   reader.readAsArrayBuffer(this.files[0]);
@@ -28,4 +71,4 @@ var map = L.map('map', {
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+  }).addTo(map);
